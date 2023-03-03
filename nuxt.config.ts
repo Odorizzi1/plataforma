cf98aa832f7c637636a8586dcc5e73e596a470c4
Zoey de Souza Pessanha <zoey.pessanha@nubank.com.br>
2023-03-03 13:54:47 -0300

    adiciona pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    vite: {
-      css: { devSourcemap: true },
-    },
-  },
   apollo: {
     autoImports: true,
-    authType: 'Bearer',
-    authHeader: 'Authorization',
-    tokenStorage: 'cookie',
+    authType: "Bearer",
+    authHeader: "Authorization",
+    tokenStorage: "cookie",
     proxyCookies: true,
     clients: {
       default: {
-        httpEndpoint: 'https://pescarte.uenf.br/api',
+        httpEndpoint: "https://pescarte.uenf.br/api",
       },
     },
   },
-  buildModules: [
-    '@nuxtjs/style-resources',
-    '@nuxtjs/google-fonts'
-  ],
-  css: ['@/assets/css/main.scss'],
+  components: true,
+  css: ["@/assets/css/main.scss"],
   modules: [
-    '@nuxtjs/apollo',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image-edge',
-    'nuxt-purgecss',
-    'magic-regexp/nuxt',
-    'nuxt-lodash',
-    'nuxt-typed-router',
-    '@vite-pwa/nuxt',
-    'nuxt-viewport',
+    "@pinia/nuxt",
+    "@nuxtjs/apollo",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "nuxt-purgecss",
+    "magic-regexp/nuxt",
+    "nuxt-lodash",
+    "nuxt-typed-router",
+    "@vite-pwa/nuxt",
+    "nuxt-viewport",
   ],
-  googleFonts: {
-    prefetch: true,
-    preconnect: true,
-    preload: true,
-    families: {
-      WorkSans: true,
-    }
-  }
-})
+});

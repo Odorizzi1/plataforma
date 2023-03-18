0b942b4c75958fb2f08028962ead8d1e8b13786a
Zoey de Souza Pessanha <zoey.pessanha@nubank.com.br>
2023-03-18 18:45:34 -0300

    adiciona componentes básicos para tela de perfil

---
+++ components/ProfileLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center justify-between w-28">
+    <span class="rounded-full bg-blue-80 h-12 w-12 flex-center">
+      <slot />
+    </span>
+    <NuxtLink :to="href" class="w-12 text-left link">
+      <Text size="btn" color="text-blue-80">
+        {{ props.label }}
+      </Text>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ href: string; label: string }>();
+</script>
---
+++ components/ProfileMenuLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="profile-menu-link">
+    <span class="flex items-center justify-center bg-white-100 h-12 w-12">
+      <slot />
+    </span>
+    <Button type="link" class="whitespace-nowrap">
+      <Text size="btn" color="text-blue-80">
+        {{ label }}
+      </Text>
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ label: string }>();
+</script>
